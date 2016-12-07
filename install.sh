92a52e3caf7e0327dbd622b8c8c937142b7b6f97
zerjioang <sergio.anguita@opendeusto.es>
2016-12-07 18:26:52 +0100

    added amazon ami installation script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Smouq installation script on Amazon AMI"
+sudo yum update -y
+echo "Installing Docker..."
+sudo yum install -y docker
+sudo service docker start
+echo "Adding Amazon user to docker group"
+sudo usermod -a -G docker ec2-user
+echo "Installing docker-compose..."
+sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
