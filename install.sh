91c3e6b85ccd7047af2e7ffae5d12b1f33533f66
EC2 Default User <ec2-user@ip-172-31-45-103.eu-west-1.compute.internal>
2016-12-18 17:34:44 +0000

    script updated

--- install.sh
+++ install.sh
@@ -10,3 +10,4 @@ echo "Installing docker-compose..."
 sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+exit
