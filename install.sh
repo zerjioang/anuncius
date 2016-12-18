05a4217054b4d8b4b622ddf0749df357e3401bb6
zerjioang <sergio.anguita@opendeusto.es>
2016-12-18 20:24:16 +0100

    changed tomcat entry html

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+echo "Installing needed dependencies..."
+echo "Installing MAVEN..."
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
+mvn --version
+echo "Removing Java 1.7"
+sudo yum remove java-1.7.0-openjdk
+echo "Installing Java 1.8"
+sudo yum install java-1.8.0-openjdk-devel
 echo "Musutruq installation script on Amazon AMI"
 sudo yum update -y
 echo "Installing Docker..."
