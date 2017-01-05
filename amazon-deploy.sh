253b8503bbf514f8d1787577b207ee811de8b006
root <root@ip-172-31-45-103.eu-west-1.compute.internal>
2017-01-05 17:34:18 +0000

    ssl cert script added

--- amazon-deploy.sh
+++ amazon-deploy.sh
@@ -1,36 +1,13 @@
 #!/bin/bash
 
-#stop docker nginx and avoid port 80 conflicts
-cd docker
-docker-compose stop balancer
-cd ..
-
-#restart local nginx
-sudo service nginx restart
-
-#request ssl cert update
-/opt/letsencrypt/certbot-auto renew
-
-#stop local nginx after request done
-sudo service nginx stop
-
-#start docker nginx again
-cd docker
-docker-compose start balancer
-cd ..
-
-#update ssl cert
-sudo chown $USER -R /etc/letsencrypt/live/
-cp /etc/letsencrypt/live/anunci.us/* /home/ec2-user/anuncius/docker/Dockerfile/nginx/ssl/
-
 #update
 sudo git pull
 
 #go to api
 
 cd api
-# /opt/apache-maven-3.3.9/bin/mvn clean
-# /opt/apache-maven-3.3.9/bin/mvn install
+#/opt/apache-maven-3.3.9/bin/mvn clean
+#/opt/apache-maven-3.3.9/bin/mvn install
 
 #mv ./api/target/*.war ./docker/Dockerfile/apps/
 
@@ -51,5 +28,6 @@ mv ./app/target/*.war ./docker/Dockerfile/tomcat/apps/
 
 #run docker
 cd docker
+
 docker-compose build web
-docker-compose restart web
+docker-compose up
---
+++ update_cert.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#stop docker nginx and avoid port 80 conflicts
+cd docker
+docker-compose stop balancer
+cd ..
+
+#restart local nginx
+sudo service nginx restart
+
+#request ssl cert update
+/opt/letsencrypt/certbot-auto renew
+
+#stop local nginx after request done
+sudo service nginx stop
+
+#start docker nginx again
+cd docker
+docker-compose start balancer
+cd ..
+
+#update ssl cert
+sudo chown $USER -R /etc/letsencrypt/live/
+cp /etc/letsencrypt/live/anunci.us/* /home/ec2-user/anuncius/docker/Dockerfile/nginx/ssl/
+
+echo "Finished"
