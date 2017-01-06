85344078af8736e9055057ae5cc686cb03c5895c
root <root@ip-172-31-46-163.eu-west-1.compute.internal>
2017-01-06 21:42:49 +0000

    moved nginx conf to config folder

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ sudo git pull
 #go to api
 cd api
 
-/home/ec2-user/anuncius/tools/apache-maven-3.3.9/bin/mvn clean
-/home/ec2-user/anuncius/tools/apache-maven-3.3.9/bin/mvn install
+../tools/apache-maven-3.3.9/bin/mvn clean
+../tools/apache-maven-3.3.9/bin/mvn install
 
 #go back
 cd ..
@@ -18,8 +18,8 @@ mv ./api/target/*.war ./docker/Dockerfile/tomcat/apps/
 #go to app
 cd app
 
-/home/ec2-user/anuncius/tools/apache-maven-3.3.9/bin/mvn clean
-/home/ec2-user/anuncius/tools/apache-maven-3.3.9/bin/mvn install
+../tools/apache-maven-3.3.9/bin/mvn clean
+../tools/apache-maven-3.3.9/bin/mvn install
 
 #go back
 cd ..
