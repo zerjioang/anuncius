8e8527e74826f5663bd0b6bba9a4121796f44b96
root <root@ip-172-31-46-163.eu-west-1.compute.internal>
2017-01-29 15:03:00 +0000

    Updated https certificate generation tools Added new domains to https certificate

--- update_cert_auto.sh
+++ update_cert_auto.sh
@@ -2,25 +2,25 @@
 
 #stop docker nginx and avoid port 80 conflicts
 cd docker
-docker-compose stop balancer
+docker-compose stop nginx
 cd ..
 
 #restart local nginx
 sudo service nginx restart
 
 #request ssl cert update
-./tools/letsencrypt/certbot-auto renew
+./tools/letsencrypt/certbot-auto renew --debug
 
 #stop local nginx after request done
 sudo service nginx stop
 
 #start docker nginx again
 cd docker
-docker-compose start balancer
+docker-compose start nginx
 cd ..
 
 #update ssl cert
 sudo chown $USER -R /etc/letsencrypt/live/
-cp /etc/letsencrypt/live/anunci.us/* /home/ec2-user/anuncius/docker/Dockerfile/nginx/ssl/
+cp /etc/letsencrypt/live/anunci.us/* /home/ec2-user/anuncius/docker/volumes/nginx/config/ssl/
 
 echo "Finished"
---
+++ update_cert_manual.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#stop docker nginx and avoid port 80 conflicts
+cd docker
+docker-compose stop nginx
+cd ..
+
+#restart local nginx
+sudo service nginx restart
+
+#request ssl cert update
+./tools/letsencrypt/certbot-auto certonly --debug
+
+#stop local nginx after request done
+sudo service nginx stop
+
+#start docker nginx again
+cd docker
+docker-compose start nginx
+cd ..
+
+#update ssl cert
+sudo chown $USER -R /etc/letsencrypt/live/
+cp /etc/letsencrypt/live/anunci.us/* /home/ec2-user/anuncius/docker/volumes/nginx/config/ssl/
+
+echo "Finished"
